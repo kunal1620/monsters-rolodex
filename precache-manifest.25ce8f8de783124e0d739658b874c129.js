f1b4f15088b38cf9f2e9f3cd5b09175237ecc419
Kunal Pawar <kunal1620@gmail.com>
2020-04-06 23:18:08 +1000

    Updates

---
+++ precache-manifest.25ce8f8de783124e0d739658b874c129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7474a2b62a864976b7f4eba30262540",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f34939fe0cfe74cdf3b",
+    "url": "/monsters-rolodex/static/css/main.57dc6fc4.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f34939fe0cfe74cdf3b",
+    "url": "/monsters-rolodex/static/js/main.4c7160f9.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
