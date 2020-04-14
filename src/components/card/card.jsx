7602c24fe3b8cb10748ca5e00556ece0d4dc4de9
Kunal Pawar <kunal1620@gmail.com>
2020-04-14 22:14:31 +1000

    Modified Readme file and minor fixes

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
     <div className="card-container">
       <img
         alt="Avatar"
-        src={`https://robohash.org/${props.monster.id}?set=set2&bgset=bg1&size=180x180`}
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
       />
       <h2>{props.monster.name}</h2>
       <p>{props.monster.email}</p>
